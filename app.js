2d153d7fc39a0b4176cfe8e0464119fbf7fbf839
zerosoul <yanggc888@163.com>
2021-06-13 00:08:15 +0800

    feat: list

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // app.js
 App({
   onLaunch() {
- 
-
     // 登录
     wx.login({
       success: res => {
@@ -11,6 +9,27 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    userList:[{
+      uuid:'1322131241243213213',
+      match:98,
+      addr:"北京",
+      position:'工程师',
+      company:'字节跳动',
+      sex:1,
+      username:'张三',
+      avator:'https://static.nicegoodthings.com/uPic/co-link/NpugaL.jpg',
+      tags:['互联网','投资','养生','跑步','互联网','投资','养生','跑步']
+    },{
+      uuid:'432432432',
+      match:98,
+      addr:"长沙",
+      position:'经理',
+      company:'字节跳动',
+      sex:2,
+      username:'赵四',
+      avator:'https://static.nicegoodthings.com/uPic/co-link/NpugaL.jpg',
+      tags:['互联网','投资','养生','跑步','互联网','养生','跑步']
+    }]
   }
 })
